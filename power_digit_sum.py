9dff82c4de0bc3f0ea18123af14d78f61fe9e2c4
Charlie <crocheford18@bsmschool.org>
2017-09-14 20:29:44 -0500

    15.3

---
+++ power_digit_sum.py
@@ -0,0 +1,2 @@
+print(sum(map(int, str(2**1000))))
+# str(2**1000) separates the digits of that number into a string of numbers.map(int, string) gives an integer to every digit so that it is now it's own number and compiles all the integers into a list so they can be summed up.
